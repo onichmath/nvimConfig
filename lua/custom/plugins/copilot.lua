eb5de9e1457f821dab8c800234940281b87ac5de
Matthew <mathonich@yahoo.com>
2023-11-19 14:33:56 -0800

    Fixed comment

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,10 +1,10 @@
--- return {
---   "github/copilot.vim",
---   lazy = false,
---   config = function()
---     vim.g.copilot_assume_mapped = true
---     vim.keymap.set('i', '<M-[>', '<Plug>(copilot-next)')
---     vim.keymap.set('i', '<M-]>', '<Plug>(copilot-previous)')
---     vim.cmd [[imap <silent><script><expr> <M-y> copilot#Accept("\<CR>")]]
---   end,
--- }
+return {
+  --   "github/copilot.vim",
+  --   lazy = false,
+  --   config = function()
+  --     vim.g.copilot_assume_mapped = true
+  --     vim.keymap.set('i', '<M-[>', '<Plug>(copilot-next)')
+  --     vim.keymap.set('i', '<M-]>', '<Plug>(copilot-previous)')
+  --     vim.cmd [[imap <silent><script><expr> <M-y> copilot#Accept("\<CR>")]]
+  --   end,
+}

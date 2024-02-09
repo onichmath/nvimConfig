041d8b5ba99e661e250dd66cd3aa2a220ea0eeea
Matthew <mathonich@yahoo.com>
2024-02-08 18:17:15 -0800

    Refactor commands and setup

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -17,3 +17,18 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagn
 -- Run Terminal
 -- vim.keymap.set('n', '<F5>', ':vnew term://zsh <CR>')
 vim.keymap.set('n', '<leader>t', ':vnew term://zsh <CR>')
+
+-- Refactoring
+vim.keymap.set("x", "<leader>re", function() require('refactoring').refactor('Extract Function') end)
+vim.keymap.set("x", "<leader>rf", function() require('refactoring').refactor('Extract Function To File') end)
+-- Extract function supports only visual mode
+vim.keymap.set("x", "<leader>rv", function() require('refactoring').refactor('Extract Variable') end)
+-- Extract variable supports only visual mode
+vim.keymap.set("n", "<leader>rI", function() require('refactoring').refactor('Inline Function') end)
+-- Inline func supports only normal
+vim.keymap.set({ "n", "x" }, "<leader>ri", function() require('refactoring').refactor('Inline Variable') end)
+-- Inline var supports both normal and visual mode
+
+vim.keymap.set("n", "<leader>rb", function() require('refactoring').refactor('Extract Block') end)
+vim.keymap.set("n", "<leader>rbf", function() require('refactoring').refactor('Extract Block To File') end)
+-- Extract block supports only normal mode
